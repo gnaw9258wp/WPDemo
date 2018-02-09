50ce2f69ff46248956eb679b806480fec1682eeb
王鹏2157号 <wangpeng2157@ct108.com>
2018-02-09 18:28:59 +0800

    fix

--- WPDemo/WPDemo/Comments/OneViewController.m
+++ WPDemo/WPDemo/Comments/OneViewController.m
@@ -7,9 +7,9 @@
 //
 
 #import "OneViewController.h"
-
-@interface OneViewController ()
-
+#import <MJRefresh/MJRefresh.h>
+@interface OneViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property (nonatomic,strong)UITableView *tableview;
 @end
 
 @implementation OneViewController
@@ -17,22 +17,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor redColor];
+    self.tableview.mj_header = [MJRefreshNormalHeader headerWithRefreshingBlock:^{
+        [self.tableview.mj_header endRefreshing];
+    }];
     // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
 }
-*/
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor colorWithRed:arc4random()%256/255.0 green:arc4random()%256/255.0 blue:arc4random()%256/255.0 alpha:1];
+    return cell;
+}
+- (UITableView *)tableview
+{
+    if (!_tableview) {
+        _tableview = [[UITableView alloc]initWithFrame:self.view.bounds style:UITableViewStylePlain];
+        _tableview.delegate = self;
+        _tableview.dataSource = self;
+        [self.view addSubview:_tableview];
+        [_tableview registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
+    }
+    return _tableview;
+}
 
 @end
